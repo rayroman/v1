27f85604f4a0bc0cfac9db283e7c5d6f8c792437
rayroman <r.roman681@me.com>
2021-07-22 20:04:38 +0200

    App: add platform to root

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
 import * as styles from './App.module.scss';
 import { Title } from './components/Title';
+import { Platform } from './components/Platform';
 
 export function App(): JSX.Element {
   return (
-    <div className={styles.root}>
-      <Title className={styles.root__title}>
-        Hello, world
-      </Title>
-    </div>
+    <Platform>
+      <div className={styles.root}>
+        <Title className={styles.root__title}>
+          Hello, world
+        </Title>
+      </div>
+    </Platform>
   );
 }
