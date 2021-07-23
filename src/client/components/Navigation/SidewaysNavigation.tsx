3c25c2591eb0552aae7922680c6fe795038954dd
rayroman <r.roman681@me.com>
2021-07-23 09:25:42 +0200

    Nav: add focus styling

--- src/client/components/Navigation/SidewaysNavigation.tsx
+++ src/client/components/Navigation/SidewaysNavigation.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import classNames from 'classnames';
 import { Link, LinkProps } from 'react-router-dom';
 import * as styles from './SidewaysNavigation.module.scss'
+import { ProviderContext } from '../Provider';
+import { FocusOrigin } from '../FocusMonitor/types';
 
 type SidewaysNavigationProps = {
   className?: string;
@@ -26,9 +28,46 @@ type LinkScrollState = {
 
 export function SidewaysNavLink(props: LinkProps<LinkScrollState>): JSX.Element {
   const { children, to, className } = props;
+
+  const provider = React.useContext(ProviderContext);
+  const [focusOrigin, setFocusOrigin] = React.useState<FocusOrigin>(null);
+
+  const unmonitor = React.useRef<() => void>(() => undefined);
+
+  const setLink = (anchor: HTMLAnchorElement | null) => {
+    if (provider && anchor) {
+      unmonitor.current = provider.focusMonitor.monitor(anchor, {
+        checkChildren: true,
+        listener: {
+          id: 'nav-link',
+          callback: setFocusOrigin,
+        }
+      });
+    }
+  };
+
+  React.useEffect(() => {
+    return () => {
+      unmonitor.current();
+    }
+  }, []);
+
   return (
     <span className={styles.root__linkContainer}>
-      <Link to={to} className={classNames(styles.root__link, className)}>
+      <Link
+        to={to}
+        className={
+          classNames(
+            styles.root__link,
+            {
+              [styles.root__linkFocused]: focusOrigin != null,
+              [styles.root__linkKeyboard]: focusOrigin === 'keyboard',
+            },
+            className,
+          )
+        }
+        ref={setLink}
+      >
         {children}
       </Link>
     </span>
