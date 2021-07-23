b65035f263a292e2975d6ce95d25c16d336807a5
rayroman <r.roman681@me.com>
2021-07-23 07:59:31 +0200

    App: add sideways navigation

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,12 +1,22 @@
 import * as React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
 import * as styles from './App.module.scss';
 import { Title } from './components/Title';
 import { Provider } from './components/Provider';
+import { SidewaysNavigation, SidewaysNavLink } from './components/Navigation/SidewaysNavigation';
 
 export function App(): JSX.Element {
   return (
     <Provider>
       <div className={styles.root}>
+        <SidewaysNavigation>
+          <SidewaysNavLink to={{ pathname: '/', state: { target: 'info' } }}>
+            about
+          </SidewaysNavLink>
+          <SidewaysNavLink to={{ pathname: '/', state: { target: 'contact' } }}>
+            contact
+          </SidewaysNavLink>
+        </SidewaysNavigation>
         <Title className={styles.root__title}>
           Hello, world
         </Title>
---
+++ src/client/components/Navigation/SidewaysNavigation.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import classNames from 'classnames';
+import { Link, LinkProps } from 'react-router-dom';
+import * as styles from './SidewaysNavigation.module.scss'
+
+type SidewaysNavigationProps = {
+  className?: string;
+  children?: React.ReactNode;
+};
+
+export function SidewaysNavigation(props: SidewaysNavigationProps): JSX.Element {
+  const { className, children } = props;
+
+  return (
+    <nav className={classNames(styles.root, className)}>
+      <div className={styles.root__container}>
+        {children}
+      </div>
+    </nav>
+  );
+}
+
+type LinkScrollState = {
+  target: string;
+}
+
+export function SidewaysNavLink(props: LinkProps<LinkScrollState>): JSX.Element {
+  const { children, to, className } = props;
+  return (
+    <span className={styles.root_linkContainer}>
+      <Link to={to} className={classNames(styles.root__link, className)}>
+        {children}
+      </Link>
+    </span>
+  );
+}
